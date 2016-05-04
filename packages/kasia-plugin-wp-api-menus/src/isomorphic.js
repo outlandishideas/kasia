38d41e86fbc865c3744b9207c4c088f6b26cfb1e
Sam Gluck <sdgluck@gmail.com>
2016-05-04 11:36:37 +0100

    Add isomorphic interface;

--- packages/kasia-plugin-wp-api-menus/src/isomorphic.js
+++ packages/kasia-plugin-wp-api-menus/src/isomorphic.js
@@ -4,6 +4,7 @@ export default function fetchData (action) {
   return (renderProps, store) => {
     const pepperoniConfig = store.wordpress.config;
     const pluginConfig = store.wordpress.config.plugins.menus;
+
     return [[fetchResource, action, pepperoniConfig, pluginConfig]];
   };
 }
