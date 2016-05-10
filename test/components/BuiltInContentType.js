085d09f89e552f564303d2600f22405537cb1922
Sam Gluck <sdgluck@gmail.com>
2016-05-10 10:34:18 +0100

    Export readable content types;

--- test/components/BuiltInContentType.js
+++ test/components/BuiltInContentType.js
@@ -4,10 +4,10 @@ jest.disableAutomock()
 
 import React, { Component } from 'react'
 
-import ContentTypes from '../../src/constants/ContentTypes'
+import { Post } from '../../src/constants/ContentTypes'
 import connectWordPress from '../../src/connect'
 
-@connectWordPress(ContentTypes.POST, (props) => props.params.id)
+@connectWordPress(Post, (props) => props.params.id)
 export default class BuiltInContentType extends Component {
   render () {
     return <div></div>
