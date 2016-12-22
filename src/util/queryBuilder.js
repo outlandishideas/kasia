932dcc631bc48e7ae552bf2fa0abcba4fb4fdfc4
Sam Gluck <sdgluck@gmail.com>
2016-12-22 12:28:19 +0000

    Use methodName of content options object and lift queryFn out of queryBuilder._deriveQuery;

--- src/util/queryBuilder.js
+++ src/util/queryBuilder.js
@@ -1,8 +1,21 @@
-import humps from 'humps'
-
 import ActionTypes from '../constants/ActionTypes'
 import getWP from '../wpapi'
 import { contentTypesManager } from '../util'
+
+function handleQueryFnError (err) {
+  // TODO log something useful, e.g. is custom content type declared?
+  throw err
+}
+
+function queryFn (wpapi, contentTypeMethodName, identifierTypeMethodName, identifierValue) {
+  try {
+    const contentTypeApi = wpapi[contentTypeMethodName]()
+    const query = contentTypeApi[identifierTypeMethodName](identifierValue)
+    return query.embed().get().catch(handleQueryFnError)
+  } catch (err) {
+    handleQueryFnError(err)
+  }
+}
 
 const queryBuilder = {}
 
@@ -24,19 +37,12 @@ export default queryBuilder
  * @returns {Function} A function to make a request to the WP-API
  */
 queryBuilder._deriveQuery = function queryBuilderDeriveQuery (contentTypeMethodName, identifier) {
-  return () => {
-    const fn = (wpapi, contentTypeMethodName, identifierTypeMethodName, identifierValue) => {
-      const contentTypeApi = wpapi[contentTypeMethodName]()
-      const query = contentTypeApi[identifierTypeMethodName](identifierValue)
-      return query.embed().get()
-    }
-    return fn(
-      getWP(),
-      contentTypeMethodName,
-      typeof identifier === 'string' ? 'slug' : 'id',
-      identifier
-    )
-  }
+  return () => queryFn(
+    getWP(),
+    contentTypeMethodName,
+    typeof identifier === 'string' ? 'slug' : 'id',
+    identifier
+  )
 }
 
 /**
@@ -51,8 +57,7 @@ queryBuilder.makeQuery = function queryBuilderMakeQuery (action) {
 
   if (ActionTypes.RequestCreatePost === action.type) {
     const options = contentTypesManager.get(contentType)
-    const methodName = humps.camelize(options.plural)
-    realQueryFn = queryBuilder._deriveQuery(methodName, identifier)
+    realQueryFn = queryBuilder._deriveQuery(options.methodName, identifier)
   } else if (ActionTypes.RequestCreateQuery === action.type) {
     realQueryFn = queryFn
   } else {
